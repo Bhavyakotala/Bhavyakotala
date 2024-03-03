4df5b7e185f65812a91bd69b030ecf69b4397fa9
Bhavyakotala <161508929+Bhavyakotala@users.noreply.github.com>
2024-03-03 18:32:37 +0530

    Add files via upload

---
+++ Task10Externaljs/biggestoftwonums.js
@@ -0,0 +1,15 @@
+ //biggest of two numbers
+ function biggestOfTwoNums(a,b)
+ {
+     if(a>b)
+     {
+         console.log(a)
+     
+     }
+     else
+     {
+         console.log(b)
+     }
+     
+     
+ }
