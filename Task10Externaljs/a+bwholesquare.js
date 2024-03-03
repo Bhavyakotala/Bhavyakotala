459ff2d04f383c6f1bdd61cb62ab8754307844e2
Bhavyakotala <161508929+Bhavyakotala@users.noreply.github.com>
2024-03-03 18:39:31 +0530

    Add files via upload

---
+++ Task10Externaljs/a+bwholesquare.js
@@ -0,0 +1,7 @@
+ //formula=a*a+b*b+2*a*b
+ function aplusbwholeSquare(a,b)
+ {
+     let ansr 
+     ansr=(a*a)+(b*b)+(2*(a*b))
+     console.log(ansr)
+ }
---
+++ Task10Externaljs/simple.js
@@ -0,0 +1,6 @@
+ //simple interest =P*T*R/100
+ function simpleIntrest(p, t, r) {
+    let result
+    result = (p * t * r) / 100
+    console.log(result)
+}
